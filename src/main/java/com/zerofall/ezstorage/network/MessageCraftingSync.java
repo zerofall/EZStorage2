487f11db5d0deeb2f9435346d04e879194960963
sblectric <sblectric@ymail.com>
2016-07-12 12:34:03 -0400

    2.1.3 hotfix add.

--- src/main/java/com/zerofall/ezstorage/network/MessageCraftingSync.java
+++ src/main/java/com/zerofall/ezstorage/network/MessageCraftingSync.java
@@ -4,7 +4,6 @@ import io.netty.buffer.ByteBuf;
 
 import java.util.List;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
